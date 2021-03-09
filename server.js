34f9c323c21d3406e8e9c475a29a4416b61f5fac
Stephen Anthony <stephenba4@gmail.com>
2021-03-08 22:24:30 -0600

    test spotify api

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+require('dotenv').config({ path: "./.env" });
 
 const app = express();
 
 var corsOptions = {
   origin: (process.env.NODE_ENV === 'local' ? "http://localhost:8080" : "https://www.nashvillebops.com")
 };
-console.log({corsOptions});
 
 app.use(cors(corsOptions));
 
@@ -17,7 +18,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ name: 'Christina' });
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: {
+      grant_type: 'client_credentials',
+    },
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Basic 27314e11bc80475cbfa7657d2055fc8c:665b3582eed4452e8f2aba58f03e2d52',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then((response) => {
+      res.json({response})
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  // res.json({ name: 'Christina' });
 });
 
 const PORT = process.env.PORT || 3000;
