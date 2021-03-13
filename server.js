63ba75ccd6d6286b256a26c0708df56d3e055f79
Stephen Anthony <stephenba4@gmail.com>
2021-03-13 14:25:19 -0600

    get all unique artists

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('dotenv').config({ path: "./.env" });
 const SpotifyWebApi = require('spotify-web-api-node');
 const _ = require('lodash');
+const { resolve } = require("url");
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_API_ID,
@@ -33,6 +34,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  
+  let artistDetails = [];
+
   spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 0 })
   .then(function(data) {
     const artistIdsArr = [];
@@ -45,7 +49,6 @@ app.get("/", (req, res) => {
     artistIds = [...artistIdsSet];
     return artistIds
   }).then(function(artistIds) {
-    let artistDetails = [];
     spotifyApi.getArtists(artistIds)
     .then(function(data) {
       data.body.artists.forEach((artist) => {
@@ -64,7 +67,317 @@ app.get("/", (req, res) => {
           }
         );
       })
-      res.json(artistDetails)
+      return artistDetails
+    }).then(function(artistDetails2) {
+      spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 25 })
+        .then(function(data) {
+          const artistIdsArr = [];
+          data.body.items.forEach((item) => {
+            item.track.artists.forEach((artist) => {
+              artistIdsArr.push(artist.id);
+            })
+          })
+          const artistIdsSet = new Set(artistIdsArr);
+          artistIds = [...artistIdsSet];
+          return artistIds
+        }).then(function(artistIds) {
+          spotifyApi.getArtists(artistIds)
+          .then(function(data) {
+            data.body.artists.forEach((artist) => {
+              const { id, name, followers, images, external_urls } = artist;
+              const totalFollowers = _.get(followers, 'total', 0);
+              const firstImage = images[0];
+              const firstImageUrl = _.get(firstImage, 'url', '');
+              const spotify = _.get(external_urls, 'spotify', '');
+              artistDetails2.push(
+                {
+                  id,
+                  artist: name,
+                  spotifyFollowers: totalFollowers,
+                  img: firstImageUrl,
+                  spotify,
+                }
+              );
+            })
+            return artistDetails2
+          }).then(function(artistDetails3) {
+            spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 50 })
+            .then(function(data) {
+              const artistIdsArr = [];
+              data.body.items.forEach((item) => {
+                item.track.artists.forEach((artist) => {
+                  artistIdsArr.push(artist.id);
+                })
+              })
+              const artistIdsSet = new Set(artistIdsArr);
+              artistIds = [...artistIdsSet];
+              return artistIds
+            }).then(function(artistIds) {
+              spotifyApi.getArtists(artistIds)
+              .then(function(data) {
+                data.body.artists.forEach((artist) => {
+                  const { id, name, followers, images, external_urls } = artist;
+                  const totalFollowers = _.get(followers, 'total', 0);
+                  const firstImage = images[0];
+                  const firstImageUrl = _.get(firstImage, 'url', '');
+                  const spotify = _.get(external_urls, 'spotify', '');
+                  artistDetails3.push(
+                    {
+                      id,
+                      artist: name,
+                      spotifyFollowers: totalFollowers,
+                      img: firstImageUrl,
+                      spotify,
+                    }
+                  );
+                })
+                return artistDetails3
+              }).then(function(artistDetails4) {
+                spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 75 })
+                .then(function(data) {
+                  const artistIdsArr = [];
+                  data.body.items.forEach((item) => {
+                    item.track.artists.forEach((artist) => {
+                      artistIdsArr.push(artist.id);
+                    })
+                  })
+                  const artistIdsSet = new Set(artistIdsArr);
+                  artistIds = [...artistIdsSet];
+                  return artistIds
+                }).then(function(artistIds) {
+                  spotifyApi.getArtists(artistIds)
+                  .then(function(data) {
+                    data.body.artists.forEach((artist) => {
+                      const { id, name, followers, images, external_urls } = artist;
+                      const totalFollowers = _.get(followers, 'total', 0);
+                      const firstImage = images[0];
+                      const firstImageUrl = _.get(firstImage, 'url', '');
+                      const spotify = _.get(external_urls, 'spotify', '');
+                      artistDetails4.push(
+                        {
+                          id,
+                          artist: name,
+                          spotifyFollowers: totalFollowers,
+                          img: firstImageUrl,
+                          spotify,
+                        }
+                      );
+                    })
+                    return artistDetails4
+                  }).then(function(artistDetails5) {
+                    spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 100 })
+                    .then(function(data) {
+                      const artistIdsArr = [];
+                      data.body.items.forEach((item) => {
+                        item.track.artists.forEach((artist) => {
+                          artistIdsArr.push(artist.id);
+                        })
+                      })
+                      const artistIdsSet = new Set(artistIdsArr);
+                      artistIds = [...artistIdsSet];
+                      return artistIds
+                    }).then(function(artistIds) {
+                      spotifyApi.getArtists(artistIds)
+                      .then(function(data) {
+                        data.body.artists.forEach((artist) => {
+                          const { id, name, followers, images, external_urls } = artist;
+                          const totalFollowers = _.get(followers, 'total', 0);
+                          const firstImage = images[0];
+                          const firstImageUrl = _.get(firstImage, 'url', '');
+                          const spotify = _.get(external_urls, 'spotify', '');
+                          artistDetails5.push(
+                            {
+                              id,
+                              artist: name,
+                              spotifyFollowers: totalFollowers,
+                              img: firstImageUrl,
+                              spotify,
+                            }
+                          );
+                        })
+                        return artistDetails5
+                      }).then(function(artistDetails6) {
+                        spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 125 })
+                        .then(function(data) {
+                          const artistIdsArr = [];
+                          data.body.items.forEach((item) => {
+                            item.track.artists.forEach((artist) => {
+                              artistIdsArr.push(artist.id);
+                            })
+                          })
+                          const artistIdsSet = new Set(artistIdsArr);
+                          artistIds = [...artistIdsSet];
+                          return artistIds
+                        }).then(function(artistIds) {
+                          spotifyApi.getArtists(artistIds)
+                          .then(function(data) {
+                            data.body.artists.forEach((artist) => {
+                              const { id, name, followers, images, external_urls } = artist;
+                              const totalFollowers = _.get(followers, 'total', 0);
+                              const firstImage = images[0];
+                              const firstImageUrl = _.get(firstImage, 'url', '');
+                              const spotify = _.get(external_urls, 'spotify', '');
+                              artistDetails6.push(
+                                {
+                                  id,
+                                  artist: name,
+                                  spotifyFollowers: totalFollowers,
+                                  img: firstImageUrl,
+                                  spotify,
+                                }
+                              );
+                            })
+                            return artistDetails6
+                          }).then(function(artistDetails7) {
+                            spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 150 })
+                            .then(function(data) {
+                              const artistIdsArr = [];
+                              data.body.items.forEach((item) => {
+                                item.track.artists.forEach((artist) => {
+                                  artistIdsArr.push(artist.id);
+                                })
+                              })
+                              const artistIdsSet = new Set(artistIdsArr);
+                              artistIds = [...artistIdsSet];
+                              return artistIds
+                            }).then(function(artistIds) {
+                              spotifyApi.getArtists(artistIds)
+                              .then(function(data) {
+                                data.body.artists.forEach((artist) => {
+                                  const { id, name, followers, images, external_urls } = artist;
+                                  const totalFollowers = _.get(followers, 'total', 0);
+                                  const firstImage = images[0];
+                                  const firstImageUrl = _.get(firstImage, 'url', '');
+                                  const spotify = _.get(external_urls, 'spotify', '');
+                                  artistDetails7.push(
+                                    {
+                                      id,
+                                      artist: name,
+                                      spotifyFollowers: totalFollowers,
+                                      img: firstImageUrl,
+                                      spotify,
+                                    }
+                                  );
+                                })
+                                return artistDetails7
+                              }).then(function(artistDetails8) {
+                                spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 175 })
+                                .then(function(data) {
+                                  const artistIdsArr = [];
+                                  data.body.items.forEach((item) => {
+                                    item.track.artists.forEach((artist) => {
+                                      artistIdsArr.push(artist.id);
+                                    })
+                                  })
+                                  const artistIdsSet = new Set(artistIdsArr);
+                                  artistIds = [...artistIdsSet];
+                                  return artistIds
+                                }).then(function(artistIds) {
+                                  spotifyApi.getArtists(artistIds)
+                                  .then(function(data) {
+                                    data.body.artists.forEach((artist) => {
+                                      const { id, name, followers, images, external_urls } = artist;
+                                      const totalFollowers = _.get(followers, 'total', 0);
+                                      const firstImage = images[0];
+                                      const firstImageUrl = _.get(firstImage, 'url', '');
+                                      const spotify = _.get(external_urls, 'spotify', '');
+                                      artistDetails8.push(
+                                        {
+                                          id,
+                                          artist: name,
+                                          spotifyFollowers: totalFollowers,
+                                          img: firstImageUrl,
+                                          spotify,
+                                        }
+                                      );
+                                    })
+                                    return artistDetails8
+                                  }).then(function(artistDetails9) {
+                                    
+                                    spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 200 })
+                                .then(function(data) {
+                                  const artistIdsArr = [];
+                                  data.body.items.forEach((item) => {
+                                    item.track.artists.forEach((artist) => {
+                                      artistIdsArr.push(artist.id);
+                                    })
+                                  })
+                                  const artistIdsSet = new Set(artistIdsArr);
+                                  artistIds = [...artistIdsSet];
+                                  return artistIds
+                                }).then(function(artistIds) {
+                                  spotifyApi.getArtists(artistIds)
+                                  .then(function(data) {
+                                    data.body.artists.forEach((artist) => {
+                                      const { id, name, followers, images, external_urls } = artist;
+                                      const totalFollowers = _.get(followers, 'total', 0);
+                                      const firstImage = images[0];
+                                      const firstImageUrl = _.get(firstImage, 'url', '');
+                                      const spotify = _.get(external_urls, 'spotify', '');
+                                      artistDetails9.push(
+                                        {
+                                          id,
+                                          artist: name,
+                                          spotifyFollowers: totalFollowers,
+                                          img: firstImageUrl,
+                                          spotify,
+                                        }
+                                      );
+                                    })
+                                    return artistDetails9
+                                  }).then(function(artistDetails10) {
+                                    spotifyApi.getPlaylistTracks('0T9TsvrNtkbbwSxj29341t', { limit: 40, offset: 225 })
+                                    .then(function(data) {
+                                      const artistIdsArr = [];
+                                      data.body.items.forEach((item) => {
+                                        item.track.artists.forEach((artist) => {
+                                          artistIdsArr.push(artist.id);
+                                        })
+                                      })
+                                      const artistIdsSet = new Set(artistIdsArr);
+                                      artistIds = [...artistIdsSet];
+                                      return artistIds
+                                    }).then(function(artistIds) {
+                                      spotifyApi.getArtists(artistIds)
+                                      .then(function(data) {
+                                        data.body.artists.forEach((artist) => {
+                                          const { id, name, followers, images, external_urls } = artist;
+                                          const totalFollowers = _.get(followers, 'total', 0);
+                                          const firstImage = images[0];
+                                          const firstImageUrl = _.get(firstImage, 'url', '');
+                                          const spotify = _.get(external_urls, 'spotify', '');
+                                          artistDetails10.push(
+                                            {
+                                              id,
+                                              artist: name,
+                                              spotifyFollowers: totalFollowers,
+                                              img: firstImageUrl,
+                                              spotify,
+                                            }
+                                          );
+                                        })
+                                        return artistDetails10
+                                      }).then(function(artistDetails11) {
+                                        const distinctArtists = _.uniqBy(artistDetails11, 'id')
+                                        res.json(distinctArtists)
+                                      })
+                                    })
+                                  })
+                                })
+                              })
+                            })
+                          })
+                        })
+                      })
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
     })
   })
 });
